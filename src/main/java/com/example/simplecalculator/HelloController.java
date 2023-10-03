0f7aea110e5f60f99c1cdc23f5a53918ccc95b87
Nicklas Kramer <kramernicklas@gmail.com>
2023-10-03 13:07:51 +0200

    Cleaner code

--- src/main/java/com/example/simplecalculator/HelloController.java
+++ src/main/java/com/example/simplecalculator/HelloController.java
@@ -27,6 +27,7 @@ public class HelloController {
         lblNumber2.setText("");
         lblOperator.setText("");
     }
+
     public void onCalculateClick(ActionEvent actionEvent) {
         number1 = Integer.parseInt(lblNumber1.getText());
         number2 = Integer.parseInt(lblNumber2.getText());
@@ -113,84 +114,73 @@ public class HelloController {
     }
 
     public void onZeroClick(ActionEvent actionEvent) {
-        if(!operatorClicked){
+        if(!operatorClicked)
             lblNumber1.setText(lblNumber1.getText() + "0");
-        }
-        else{
+        else
             lblNumber2.setText(lblNumber2.getText() + "0");
-        }
     }
+
     public void onOneClick(ActionEvent actionEvent) {
-        if(!operatorClicked){
+        if(!operatorClicked)
             lblNumber1.setText(lblNumber1.getText() + "1");
-        }
-        else{
+        else
             lblNumber2.setText(lblNumber2.getText() + "1");
-        }
     }
+
     public void onTwoClick(ActionEvent actionEvent) {
-        if(!operatorClicked){
+        if(!operatorClicked)
             lblNumber1.setText(lblNumber1.getText() + "2");
-        }
-        else{
+        else
             lblNumber2.setText(lblNumber2.getText() + "2");
-        }
     }
+
     public void onThreeClick(ActionEvent actionEvent) {
-        if(!operatorClicked){
+        if(!operatorClicked)
             lblNumber1.setText(lblNumber1.getText() + "3");
-        }
-        else{
+        else
             lblNumber2.setText(lblNumber2.getText() + "3");
-        }
     }
+
     public void onFourClick(ActionEvent actionEvent) {
-        if(!operatorClicked){
+        if(!operatorClicked)
             lblNumber1.setText(lblNumber1.getText() + "4");
-        }
-        else{
+        else
             lblNumber2.setText(lblNumber2.getText() + "4");
-        }
     }
+
     public void onFiveClick(ActionEvent actionEvent) {
-        if(!operatorClicked){
+        if(!operatorClicked)
             lblNumber1.setText(lblNumber1.getText() + "5");
-        }
-        else{
+        else
             lblNumber2.setText(lblNumber2.getText() + "5");
-        }
     }
+
     public void onSixClick(ActionEvent actionEvent) {
-        if(!operatorClicked){
+        if(!operatorClicked)
             lblNumber1.setText(lblNumber1.getText() + "6");
-        }
-        else{
+        else
             lblNumber2.setText(lblNumber2.getText() + "6");
-        }
     }
+
     public void onSevenClick(ActionEvent actionEvent) {
-        if(!operatorClicked){
+        if(!operatorClicked)
             lblNumber1.setText(lblNumber1.getText() + "7");
-        }
-        else{
+        else
             lblNumber2.setText(lblNumber2.getText() + "7");
-        }
     }
+
     public void onEightClick(ActionEvent actionEvent) {
-        if(!operatorClicked){
+        if(!operatorClicked)
             lblNumber1.setText(lblNumber1.getText() + "8");
-        }
-        else{
+        else
             lblNumber2.setText(lblNumber2.getText() + "8");
-        }
     }
+
     public void onNineClick(ActionEvent actionEvent) {
-        if(!operatorClicked){
+        if(!operatorClicked)
             lblNumber1.setText(lblNumber1.getText() + "9");
-        }
-        else{
+        else
             lblNumber2.setText(lblNumber2.getText() + "9");
-        }
     }
 
     public void onConvertClick(ActionEvent actionEvent) {
